f5e6d1eaec838bfde2c36ffdf8e3fb017a80a12f
g2a-world <g2a-world>
2023-10-15 21:04:08 +0000

    committing changes to feature_branch_01

--- data_processing/ingest_student_csv.py
+++ data_processing/ingest_student_csv.py
@@ -7,5 +7,3 @@ csv_student = spark.read \
             .csv("/mnt/formula1dlg2a/student/student_day_1.csv")
 
 display(csv_student)
-# Get me total number of rows in this ingetstion processes.
-csv_student.count()
