95626839c408bf174e5dda6585ef9a2a713e7751
g2a-world <g2a-world>
2023-10-15 04:05:29 +0000

    first commit

---
+++ ingest_student_csv.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+csv_student = spark.read \
+    .option("inferSchema", True) \
+        .option("header", True) \
+            .csv("/mnt/formula1dlg2a/student/student_day_1.csv")
+
+display(csv_student)
